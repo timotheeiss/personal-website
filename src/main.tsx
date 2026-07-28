54446dcf7ec8459452de98179d311d81d1ddf542
Tim Issenmann <timothee.issenmann@gmail.com>
2026-07-28 17:38:46 +0100

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import '@fontsource-variable/inter/wght.css'
 import '@fontsource-variable/lora/wght.css'
 import App from './App'
@@ -8,5 +9,6 @@ import './styles.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
